ef0131f4786ed4895e4e4d9f30bfcc3c0a44e1a0
KactuX Team <team@kactux.com>
2026-01-07 01:26:42 -0300

    chore: Clean extraneous debug files from front/ directory

--- front/test_include.php
+++
@@ -1,9 +0,0 @@
-<?php
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
-echo "Start include...\n";
-if (!defined('GLPI_ROOT')) {
-    define('GLPI_ROOT', dirname(dirname(dirname(__DIR__))));
-}
-require (GLPI_ROOT . '/inc/includes.php');
-echo "Include success.\n";
