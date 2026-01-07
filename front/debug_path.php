63c5176a38ad59dbc467e9d80a5d9de7c68a11d8
KactuX Team <team@kactux.com>
2026-01-07 01:27:11 -0300

    chore: Clean extraneous debug files from front/ directory in glpi-10 branch

--- front/debug_path.php
+++
@@ -1,6 +0,0 @@
-<?php
-include ('../../../inc/includes.php');
-header('Content-Type: text/plain');
-echo "GLPI Root Doc: " . $CFG_GLPI["root_doc"] . "\n";
-echo "Profile Form Path: " . $CFG_GLPI["root_doc"] . "/plugins/flowbpmn/front/profile.form.php\n";
-echo "User ID: " . Session::getLoginUserID() . "\n";
